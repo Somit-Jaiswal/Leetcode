a9ea6a5f3852340e16f99b638d6b07561ed16726
Somit Jaiswal <122624177+Somit-Jaiswal@users.noreply.github.com>
2023-10-23 16:19:06 +0530

    Time: 1 ms (76.31%), Space: 39.4 MB (58.82%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,20 +1,12 @@
-class Solution {
-    public boolean isPowerOfFour(int n) {
-        return n == 1 ||
-            n == 1 << 2 ||
-            n == 1 << 4 ||
-            n == 1 << 6 ||
-            n == 1 << 8 ||
-            n == 1 << 10 ||
-            n == 1 << 12 ||
-            n == 1 << 14 ||
-            n == 1 << 16 ||
-            n == 1 << 18 ||
-            n == 1 << 20 ||
-            n == 1 << 22 ||
-            n == 1 << 24 ||
-            n == 1 << 26 ||
-            n == 1 << 28 ||
-            n == 1 << 30;
+public class Solution {
+    public boolean isPowerOfFour(int n) {  
+        for (int i = 0; i <= 15; i++) {
+            int powerOfFour = (int) Math.pow(4, i);
+              if (powerOfFour == n)
+                return true;
+             if (powerOfFour > n)
+                return false;
+        }  
+        return false;
     }
 }
