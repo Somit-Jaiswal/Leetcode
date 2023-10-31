5c08ab47116971521fe2a262a318b814d7f6560a
Somit Jaiswal <122624177+Somit-Jaiswal@users.noreply.github.com>
2023-10-31 23:25:57 +0530

    Time: 1 ms (99.57%), Space: 39.8 MB (88.88%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,35 +1,19 @@
 class Solution {
-    String convert(String s)
-    {
-        char prevCh = s.charAt(0);
-        int prevCount = 1; //stores count of consecutive same digits
-        StringBuilder ans = new StringBuilder();
-        for (int i = 1; i < s.length(); i++)
-        {
-            char currCh = s.charAt(i);
-            if (currCh != prevCh)   
-            {
-                ans.append(Integer.toString(prevCount));
-                ans.append(prevCh);
-                prevCount = 1; //reset count
-                prevCh = currCh;
-            }
-            else prevCount++;
-        }
-        ans.append(Integer.toString(prevCount));
-        ans.append(prevCh);
-        return ans.toString();
-    }
-    public String countAndSay(int n) 
-    {
-        String prevStr = "1";
-        for (int i = 2; i <= n; i++)
-        {
-            String cnsNum = convert(prevStr); //cns  = "count and say Num"
-            prevStr = cnsNum; //remember (dp) the previous built string
-        }
-        return prevStr;
-        
-        
-    }
+    public String countAndSay(int n) {
+if (n == 1) return "1";
+String s = countAndSay(n - 1);
+StringBuilder sb = new StringBuilder();
+int count = 1;
+for (int i = 1; i < s.length(); i++) {
+if (s.charAt(i) == s.charAt(i - 1)) count++;
+else{
+sb.append(count).append(s.charAt(i - 1));
+count = 1;
+}
+}
+sb.append(count).append(s.charAt(s.length() - 1));
+return sb.toString();
+}
+
+
 }
