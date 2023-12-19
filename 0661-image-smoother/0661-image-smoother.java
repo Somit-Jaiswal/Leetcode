fcda84bcbd32a02da0227960cd2a1a578bb0f351
Somit Jaiswal <122624177+Somit-Jaiswal@users.noreply.github.com>
2023-12-19 10:57:05 +0530

    Time: 7 ms (50.42%), Space: 44.7 MB (33.89%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.java
+++ 0661-image-smoother/0661-image-smoother.java
@@ -2,37 +2,24 @@ class Solution {
     public int[][] imageSmoother(int[][] img) {
         int rows = img.length;
         int cols = img[0].length;
-
-        // Apply the average function to each pixel in the image matrix
         int[][] result = new int[rows][cols];
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                result[r][c] = calculateAverage(img, r, c, rows, cols);
+
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                int total_sum = 0;
+                int count = 0;
+
+                for (int l = Math.max(0, i-1); l < Math.min(rows, i+2); ++l) {
+                    for (int k = Math.max(0, j-1); k < Math.min(cols, j+2); ++k) {
+                        total_sum += img[l][k];
+                        count += 1;
+                    }
+                }
+
+                result[i][j] = total_sum / count;
             }
         }
 
-        return result;        
+        return result;
     }
-
-    private int calculateAverage(int[][] img, int r, int c, int rows, int cols) {
-        int total = 0;
-        int count = 0;
-
-        // Define the boundaries for the neighboring pixels
-        int top = Math.max(0, r - 1);
-        int bottom = Math.min(rows, r + 2);
-        int left = Math.max(0, c - 1);
-        int right = Math.min(cols, c + 2);
-
-        // Iterate over the neighboring pixels and calculate the sum and count
-        for (int row = top; row < bottom; row++) {
-            for (int col = left; col < right; col++) {
-                total += img[row][col];
-                count += 1;
-            }
-        }
-
-        // Calculate and return the average value for the pixel
-        return total / count;
-    }    
 }
