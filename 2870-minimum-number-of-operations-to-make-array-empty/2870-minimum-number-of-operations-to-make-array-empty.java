3e4a421e4edc6a1bf548e49d85bb7313fc349d70
Somit Jaiswal <122624177+Somit-Jaiswal@users.noreply.github.com>
2024-01-04 23:25:13 +0530

    Time: 20 ms (56.98%), Space: 63.4 MB (6.04%) - LeetHub

---
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        Map<Integer,Integer> nm=new HashMap<>();
+        for(int i:nums)
+        {
+            nm.put(i, nm.getOrDefault(i,0)+1);
+        }
+        int ans=0;
+        for(int i:nm.keySet())
+        {
+            if(nm.get(i)==1)
+            {
+                return -1;
+            }
+            int size=nm.get(i);
+            ans=ans+(size/3)+(size % 3 != 0 ? 1 : 0);
+        }
+        return ans;
+    }
+}
